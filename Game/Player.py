87d8e66898e1ae24070c594b6bb8651cda41124a
v15hv4 <vishva2912@gmail.com>
2020-11-03 03:10:02 +0530

    Add maze wall collisions

--- Game/Player.py
+++ Game/Player.py
@@ -1,24 +1,54 @@
 import pygame
 
+# singular point 'p' and a line defined by points 'a' and 'b'
+def collides(a, b, p, offset):
+    x1, y1 = a
+    x2, y2 = b
+    x0, y0 = p
+    d_ap = ((x1 - x0) ** 2 + (y1 - y0) ** 2) ** 0.5
+    d_bp = ((x2 - x0) ** 2 + (y2 - y0) ** 2) ** 0.5
+    d_ab = ((x2 - x1) ** 2 + (y2 - y1) ** 2) ** 0.5
+    return not ((d_ap + d_bp) > d_ab)
+
 
 class Player:
-    def __init__(self, screen, start_position, width, color=(255, 255, 255), speed=0.5):
+    def __init__(self, screen, start_position, width, color=(255, 255, 255), speed=0.2):
         self.screen = screen
         self.position = list(start_position)
         self.width = width
         self.color = color
         self.speed = speed
 
-    def move_up(self):
+    def move_up(self, WALLS):
+        x, y = self.position
+        for wall in WALLS:
+            if collides(wall.a, wall.b, (x, y - self.speed), 1):
+                return
+
         self.position[1] -= self.speed
 
-    def move_down(self):
+    def move_down(self, WALLS):
+        x, y = self.position
+        for wall in WALLS:
+            if collides(wall.a, wall.b, (x, y + self.speed), 1):
+                return
+
         self.position[1] += self.speed
 
-    def move_left(self):
+    def move_left(self, WALLS):
+        x, y = self.position
+        for wall in WALLS:
+            if collides(wall.a, wall.b, (x - self.speed, y), 1):
+                return
+
         self.position[0] -= self.speed
 
-    def move_right(self):
+    def move_right(self, WALLS):
+        x, y = self.position
+        for wall in WALLS:
+            if collides(wall.a, wall.b, (x + self.speed, y), 1):
+                return
+
         self.position[0] += self.speed
 
     def draw(self):
--- Game/Wall.py
+++ Game/Wall.py
@@ -4,12 +4,13 @@ import pygame
 class Wall:
     def __init__(self, screen, a, b, color=(255, 255, 255), width=2):
         self.screen = screen
-        self.point_a = a
-        self.point_b = b
-        self.position = (((a[0] + b[0]) / 2), (a[1] + b[1] / 2))
-        self.length = abs((a[0] - b[0]) + (a[1] - b[1]))
+        self.a = a
+        self.b = b
         self.color = color
         self.width = width
 
+    def __eq__(self, other):
+        return self.a == other.a and self.b == other.b
+
     def draw(self):
-        pygame.draw.line(self.screen, self.color, self.point_a, self.point_b, self.width)
+        pygame.draw.line(self.screen, self.color, self.a, self.b, self.width)
