790083d9a34ac2b34a5b91b3a669338552652015
smartypants042 <tanishqchaudhary511@gmail.com>
2020-11-13 10:54:31 +0530

    Updated to be Theoritically Accurate and Practical

--- Advanced/RL/reward.py
+++ Advanced/RL/reward.py
@@ -1,17 +1,32 @@
-from helper import state_to_cell
+from helper import state_to_cell, get_next_state
 from mirage_helper import check
-from Q_Learning import ACTION_MAPPER
 
-def reward(maze, state):
-    return 1000 if state == (maze.num_rows*maze.num_columns - 1) else 0
+INF = float('inf')
+ACTION_MAPPER = {
+        0: 'N',
+        1: 'S',
+        2: 'W',
+        3: 'E'
+    }
+
+def reward(maze, state, action):
+    next_state = get_next_state(maze, state, action)
+    x, y = state_to_cell(maze, state)
+
+    if maze.grid[x][y].neighbors[ACTION_MAPPER[action]] == INF:
+        return -10000
+    elif next_state == (maze.num_rows*maze.num_columns - 1):
+        return 1000
+    else:
+        return 0
 
 def reward_mirage(maze, mirage, state, action):
-    if action == 4:
-        x, y = state_to_cell(state)
-        direction = ACTION_MAPPER[action]
+    if action >= 4:
+        x, y = state_to_cell(maze, state)
+        direction = ACTION_MAPPER[action-4]
         if check(mirage, maze, x, y, direction):
             return 100
         else:
             return -100
     else:
-        return reward(maze, state)
+        return reward(maze, state, action)
